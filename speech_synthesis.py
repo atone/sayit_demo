82a01c8ff88d603d0bedd8f1b2148c6f689c6fc8
Naitong Yu <ntyu@baai.ac.cn>
2024-11-26 18:06:01 +0800

    convert tab to spaces

--- speech_synthesis.py
+++ speech_synthesis.py
@@ -2,14 +2,14 @@ import os
 import azure.cognitiveservices.speech as speechsdk
 
 def speak_text(text):
-	speech_config = speechsdk.SpeechConfig(subscription=os.getenv('AZURE_SPEECH_KEY'), region=os.getenv('AZURE_SPEECH_REGION'))
-	audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
-	speech_config.speech_synthesis_voice_name='zh-CN-XiaoxiaoMultilingualNeural'
+    speech_config = speechsdk.SpeechConfig(subscription=os.getenv('AZURE_SPEECH_KEY'), region=os.getenv('AZURE_SPEECH_REGION'))
+    audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+    speech_config.speech_synthesis_voice_name='zh-CN-XiaoxiaoMultilingualNeural'
 
-	speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 
-	result = speech_synthesizer.speak_text_async(text).get()
-	if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
-		print("语音合成完成: [{}]".format(text))
-	elif result.reason == speechsdk.ResultReason.Canceled:
-		print("语音合成取消: {}".format(result.cancellation_details.reason))
+    result = speech_synthesizer.speak_text_async(text).get()
+    if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+        print("语音合成完成: [{}]".format(text))
+    elif result.reason == speechsdk.ResultReason.Canceled:
+        print("语音合成取消: {}".format(result.cancellation_details.reason))
